82379ab8ea17128889454578c14e3a0b6c8eab8f
Ahmed Zaghloul <ahmed@Ahmeds-MacBook-Air.local>
2022-09-01 08:59:28 +0200

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function login(){
+  //authentic login info
   const authUsrName= "admin";
   const authPswd= "123";
+  //selectors
   const usrNameInput= document.getElementById("usrName").value;
   const usrPswdInput= document.getElementById("pswd").value;
   // const notif = document.getElementById("notification");
@@ -9,13 +11,10 @@ function login(){
     document.write("<h2>Welcome</h2>");
     // var text = document.createTextNode("Welcome");
     // notif.appendChild(text);
-    
-
   }else{
     document.write("Not Registered");
     // var text = document.createTextNode("Not Registered");
     // notif.appendChild(text);
-
   }
 
 }
